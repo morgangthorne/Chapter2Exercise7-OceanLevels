015a512b177d61b44b73d771d20d1e32bfd809b8
Morgan Thorne <mt34452@email.vccs.edu>
2025-03-02 22:11:56 -0500

    Final Code

--- Chapter2Exercise7-OceanLevels.cpp
+++ Chapter2Exercise7-OceanLevels.cpp
@@ -3,26 +3,34 @@ Filename: Chapter2Exercise7-OceanLevels.cpp
 Programmer: Morgan Thorne
 Date: February 2025
 Requirements:
-
-
-
+Write a program that displays:
+-The number of millimeters higher than the current level that the ocean's level will be in 5 years.
+-The number of millimeters higher than the current level that the ocean's level will be in 7 years.
+-The number of millimeters higher than the current level that the ocean's level will be in 10 years.
 */
 
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float currentoceanlevel = 0;
+	float currentoceansrise = 1.5;
+	float oceanlevel5years = currentoceansrise * 5;
+	float oceanlevel7years = currentoceansrise * 7;
+	float oceanlevel10years = currentoceansrise * 10;
+
+	cout << "Every year the ocean level rises by 1.5 millimeters." << endl;
+	cout << "Lets say that the oceans level is at 0 millimeters right now." << endl;
+	cout << "Current ocean level: " << currentoceanlevel << "mm" << endl;
+	cout << "The ocean's level will have risen by " << oceanlevel5years << "mm in 5 years." << endl;
+	cout << "The ocean's level will have risen by " << oceanlevel7years << "mm in 7 years." << endl;
+	cout << "The ocean's level will have risen by " << oceanlevel10years << "mm in 10 years." << endl;
+
+
+
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
